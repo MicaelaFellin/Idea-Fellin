fcd2629b748139432a621b0024a4eaf835fe3d53
MicaelaFellin <109999065+MicaelaFellin@users.noreply.github.com>
2022-10-23 22:19:16 -0300

    agregando cards en jscript (#2)
    * agreganddo cards en jscript
    
    * crear lista de compras  y eliminar comentarios
    
    * add localStorage y calculco total
    
    * agregando comentarios
    
    * crear producto con metodo, input search y boton borrar todo carrito
    
    * mas comentarios

--- js/script.js
+++ js/script.js
@@ -1,177 +1,331 @@
-// Productos - Precios - Stock
+// ARRAY DE PRODUCTOS
 
-/* VARIABLES ANTES DE APLICAR OBJETOS
+const listaProductos =
+    [
+        {
+            "id": 1,
+            "nombre": "Escritorio Regulable Blanco",
+            "img": "media/regulable-blanco.jpg",
+            "desc": "Realizado en hierro blanco y madera.",
+            "precio": 60000,
+            "stock": 5
+        },
+        {
+            "id": 2,
+            "nombre": "Escritorio Madera",
+            "img": "media/Escritorio-paraiso.jpg",
+            "desc": "Realizado en madera y melamina beige.",
+            "precio": 80000,
+            "stock": 5
+        },
+        {
+            "id": 3,
+            "nombre": "Escritorio Negro",
+            "img": "media/escritorio negro.jpg",
+            "desc": "Realizado en madera y estructura de hierro negro.",
+            "precio": 200000,
+            "stock": 5
+        },
+        {
+            "id": 4,
+            "nombre": "Escritorio Gris",
+            "img": "media/escritorio-gris.jpg",
+            "desc": "Realizado en madera y estructura de hierro gris.",
+            "precio": 10000,
+            "stock": 10
+        },
+        {
+            "id": 5,
+            "nombre": "Escritorio Blanco",
+            "img": "media/escritorio blanco.jpg",
+            "desc": "Realizado en madera y melamina blanca.",
+            "precio": 90000,
+            "stock": 10
+        },
+        {
+            "id": 6,
+            "nombre": "Escritorio Regulable Madera",
+            "img": "media/regulable-madera.jpg",
+            "desc": "Realizado en madera y estructura de hierro negro.",
+            "precio": 50000,
+            "stock": 10
+        },
 
-// Escritorios clásicos
-let nombreProducto1 = "Escritorio clásico"
-let precioProducto1 = 60000
-let stockProducto1 = 5
+    ]
+// CREAR PRODUCTO Y AGREGARLO A LA LISTA DE PRODUCTOS
 
-// Escritorios estilo industrial
-let nombreProducto2 = "Escritorio estilo industrial"
-let precioProducto2 = 80000
-let stockProducto2 = 5
+function agregarProducto(nombre, img, desc, precio, stock) {
+    let producto = {}
+    producto['id'] = listaProductos.length + 1
+    producto['nombre'] = nombre
+    producto['img'] = img
+    producto['desc'] = desc
+    producto['precio'] = precio
+    producto['stock'] = stock
+    listaProductos.push(producto)
+}
 
-// Escritorios regulables
-let nombreProducto3 = "Escritorio regulable en altura"
-let precioProducto3 = 200000
-let stockProducto3 = 3
+agregarProducto('Escritorio en L','media/esquinero.jpg','Esquinero',70000,5)
 
+// OBTENEMOS ELEMENTOS DEL HTML (CARDS-CARRITO-TOTAL)
+
+let catalog = document.getElementById('itemList')
+let cart = document.getElementById('cart')
+let total= document.getElementById('total')
+let borrarCarrito = document.getElementById('borrarCarrito')
+borrarCarrito.addEventListener('click',borrarTodo)
+let searchInput = document.getElementById('searchInput')
+searchInput.addEventListener('input',buscar)
+
+// BOTON PARA ELIMINAR TODOS LOS PRODUCTOS DEL CARRITO
+
+function borrarTodo(event){
+    cartList = []
+    saveCartToStorage()
+    renderCart()
+}
+
+let listaProductosFiltrada = listaProductos // VARIABLE PARA FILTRAR LISTA DE PRODUCTOS
+
+// MANEJADOR DE EVENTO INPUT
+
+function buscar(event) {
+    if (searchInput.value !== '') {
+        listaProductosFiltrada = [] // BORRA LOS ELEMENTOS PARA QUE NO SE REPITAN EN LA BUSQUEDA POR CADA PALABRA
+
+        let searchList = searchInput.value.toLowerCase().split(" ")
+
+        searchList.forEach((palabra) => {
+
+            if (palabra !== '') {
+                listaProductosFiltrada = listaProductosFiltrada.concat(listaProductos.filter((prod) => {
+                    return prod.desc.toLowerCase().search(palabra) >= 0
+                }))
+
+            }
+
+        })
+    } else {
+        listaProductosFiltrada = listaProductos
+    }
+    listaProductosFiltrada = [...new Set(listaProductosFiltrada)]
+    listaProductosFiltrada.sort((a, b) => a.id - b.id) //ordenar lista segun Id
+    renderListaProductos(listaProductosFiltrada)
+}
+
+// LISTA DE CARRITO
+
+let cartList = []
 let precioTotal = 0
-*/
 
-// Objetos y Función constructora
+// CARGA DE STORAGE Y ACTUALIZACIÓN DEL CARRITO
 
-function Producto(nombre, precio, stock){
-    this.nombre = nombre;
-    this.precio = precio;
-    this.stock = stock;
-    // Agregando método para hacer cálculo de stock
+renderListaProductos(listaProductos)
+loadCartFromStorage()
+renderCart()
+
+// CREANDO LAS CARDS DESDE JS
+
+function renderListaProductos(lista){
+    catalog.innerHTML= ''
+
+    lista.forEach((prod) => {
+
+//EJEMPLO DE CARD DESDE EL HTML
+
+    // <div class="col-sm-12 col-md-6 col-xl-4"> //container
+    //     <div class="card mb-3 m-3"> //margen
+    //         <img src="media/regulable-blanco.jpg" class="card-img-top" alt="Regulable Blanco"> //imagen
+    //         <div class="card-body"> //cardBody
+    //             <h5 class="card-title text-center">Escritorio Regulable Blanco</h5> //title
+    //             <p class="card-text">Realizado en hierro blanco y madera.</p> //text
+    //         </div>
+    //         <div class="card-footer text-center"> //footer
+    //             <button type="button" class="btn btn-primary btn-sm">Comprar</button> //button
+    //         </div>
+    //     </div>
+    // </div>
+
+    //Page Frame
+    let container = document.createElement('div')
+    container.classList.add('col-sm-12', 'col-md-6', 'col-xl-4')
+    //Card Frame
+    let margen = document.createElement('div')
+    margen.classList.add('card', 'mb-3', 'm-3')
+    //Image
+    let imagen = document.createElement('img')
+    imagen.classList.add('card-img-top')
+    imagen.setAttribute('src', prod.img)
+    imagen.setAttribute('alt', prod.nombre)
+    //Card body
+    let cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+    //Card Title
+    let title = document.createElement('h5')
+    title.classList.add('card-title', 'text-center')
+    title.innerText = `${prod.nombre}`
+    //Card text
+    let text = document.createElement('p')
+    text.classList.add('card-text')
+    text.innerText = prod.desc
+    //Card Precio
+    let precio = document.createElement('p')
+    precio.classList.add('card-text','cardPrecio')
+    precio.innerText = `$${prod.precio.toLocaleString()}`
+    // footer
+    let footer = document.createElement('div')
+    footer.classList.add('card-footer', 'text-center')
+
+    //Button
+    let button = document.createElement('button')
+    button.classList.add('btn', 'btn-primary', 'btn-sm')
+    button.innerText = 'Comprar'
+    button.type = 'button'
+    button.setAttribute('productid', prod.id)
+    button.setAttribute('id', `btnComprar-${prod.id}`)
+    button.addEventListener('click', addItem)
+
+    footer.append(button)
+    cardBody.append(title)
+    cardBody.append(text)
+    cardBody.append(precio)
+    margen.append(imagen)
+    margen.append(cardBody)
+    margen.append(footer)
+    container.append(margen)
+    catalog.append(container)
+})
+}
+// MANEJADOR DE EVENTO BOTON COMPRAR Y GUARDADO EN STORAGE
+
+function addItem(event) {
+    cartList.push(event.target.getAttribute('productid'))
+    renderCart()
+    saveCartToStorage()
+}
+
+
+// ACTUALIZAR CARRITO
+
+function renderCart() {
+    cart.innerHTML = ''
+
+    let cartElementIdList = [...new Set(cartList)] // NUEVO ARRAY SIN PRODUCTOS REPETIDOS
     
-    this.restarStock = function(cantidad){
-        this.stock -= cantidad
+    cartElementIdList = cartElementIdList.sort((a, b) => a - b) //Ordenar elementos segun ID
+
+    cartElementIdList.forEach((cartElementId) => { 
+        let item = listaProductos.filter((producto) => {
+            return producto.id === parseInt(cartElementId)
+        })
+        let cantidad = cartList.reduce((total, id) => {
+            return id === cartElementId ? total += 1 : total
+        }, 0)
+
+        //Page Frame
+        let container = document.createElement('div')
+        container.classList.add('row', 'g-0')
+        //Image Frame
+        let imageFrame = document.createElement('div')
+        imageFrame.classList.add('col-md-4')
+        //Image
+        let imagen = document.createElement('img')
+        imagen.classList.add('card-img-top')
+        imagen.setAttribute('src', item[0].img)
+        imagen.setAttribute('alt', item[0].nombre)
+        //Card body
+        let bodyFrame = document.createElement('div')
+        bodyFrame.classList.add('col-md-8')
+        let cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+        //Card Title
+        let title = document.createElement('h5')
+        title.classList.add('card-title', 'text-center')
+        title.innerText = `${cantidad} x ${item[0].nombre}`
+        //Card text
+        let text = document.createElement('p')
+        text.classList.add('card-text')
+        text.innerText = `$${(item[0].precio*cantidad).toLocaleString()}`
+
+        //Button frame 1
+        let buttonFrame = document.createElement('div')
+        buttonFrame.classList.add('col-md-4')
+
+        //Button
+        let button = document.createElement('button')
+        button.classList.add('btn', 'btn-danger', 'btn-sm')
+        button.innerText = 'Eliminar Todo'
+        button.type = 'button'
+        button.dataset.id = item[0].id
+        button.addEventListener('click',eliminarTodos)
+        buttonFrame.append(button)
+
+      //Button frame 2
+      let buttonFrame2 = document.createElement('div')
+      buttonFrame2.classList.add('col-md-4')
+
+      //Button2
+      let button2 = document.createElement('button')
+      button2.classList.add('btn', 'btn-danger', 'btn-sm')
+      button2.innerText = 'Remover'
+      button2.type = 'button'
+      button2.dataset.id = item[0].id
+      button2.addEventListener('click',removerElemento)
+      buttonFrame2.append(button2)
+
+        cardBody.append(title)
+        cardBody.append(text)
+
+        imageFrame.append(imagen)
+        bodyFrame.append(cardBody)
+        container.append(imageFrame)
+        container.append(bodyFrame)
+        container.append(buttonFrame2)
+        container.append(buttonFrame)
+        cart.append(container)
+
+    })
+    total.innerText = `Total: $${calcularTotal().toLocaleString()}`
+}
+
+// MANEJADOR DE EVENTO PARA ELIMINAR TODOS LOS PRODUCTOS DEL CARRITO
+
+function eliminarTodos(event) {
+    cartList = cartList.filter((item) => {
+        return item != event.target.dataset.id
+    })
+
+    renderCart()
+    saveCartToStorage()
+}
+
+// MANEJADOR DE EVENTO PARA ELIMINAR 1 UNIDAD DE PRODUCTO
+
+function removerElemento(event) {
+    cartList.splice(cartList.indexOf(event.target.dataset.id), 1)
+    renderCart()
+    saveCartToStorage()
+}
+
+// CALCULO DE PRECIO TOTAL DE COMPRA
+
+function calcularTotal() {
+    return cartList.reduce((total, id) => {
+        let item = listaProductos.filter((producto) => {
+            return producto.id === parseInt(id)
+        })
+        return total + item[0].precio
+    },0)
+}
+
+// JSON Y STORAGE
+
+function saveCartToStorage(){
+    localStorage.setItem('cartList', JSON.stringify(cartList))
+}
+
+function loadCartFromStorage(){
+    if(localStorage.getItem('cartList') !== null){
+        cartList = JSON.parse(localStorage.getItem('cartList'))
     }
 }
-
-let Producto1 = new Producto("Escritorio clasico", 60000, 5)
-let Producto2 = new Producto("Escritorio estilo industrial", 80000, 5)
-let Producto3 = new Producto("Escritorio regulable en altura", 200000, 3)
-
-// Arrays
-
-let listaProductos = [Producto1, Producto2, Producto3]
-
-// Método de array
-console.log(listaProductos.length)
-
-/* Forma para agregar productos nuevos con stock a la listaNombres
-let listaNombres = []
-for(const prod of listaProductos){
-    if(prod.stock > 0){
-        listaNombres.push(prod.nombre)
-}
-}*/
-
-// Funciones de orden superior y método de búsqueda de arrays FILTER / MAP / SOME
-
-let listaProductosConStock = listaProductos.filter((prod) => prod.stock > 0) //En nuestro caso los stocks son mayores a cero, por lo tanto se muestran en el prompt.
-
-let listaNombres = listaProductosConStock.map((prod) => prod.nombre)
-
-let existe = listaProductos.some (producto => producto.nombre === "Escritorio blanco") 
-
-console.log (existe) // Como en nuestro inventario no tenemos disponible escritorio blanco, en consola nos devuelve false.
-
-let precioTotal = 0
-
-// Objetos
-
-/*let Producto1 = {
-    nombre: "Escritorio clásico",
-    precio: 60000,
-    stock: 5,
-}
-
-let Producto2 = {
-    nombre: "Escritorio estilo industrial",
-    precio: 80000,
-    stock: 5,
-}
-
-let Producto3 = {
-    nombre: "Escritorio regulable en altura",
-    precio: 200000,
-    stock: 3,
-}*/
-
-
-// Mensaje de Bienvenida
-// Aplicando método Join al array listaNombres
-
-alert("Bienvenidos a The Wood Store, estos son nuestros escritorios disponibles: \n - " + listaNombres.join ("\n - "))
-
-// Ciclo de compra con WHILE y condicional IF
-
-function precio(cantidad, precio){
-    precioTotal += (cantidad * precio)
-}
-
-let opcion = prompt("Ingrese que escritorio desea comprar: \n- Escritorio clásico: 1 \n- Escritorio estilo industrial: 2 \n- Escritorio regulable en altura: 3 \n- Salir: 4")
-
-while(opcion != "4"){
-
-    if(opcion == "1"){
-        let cantidadProducto1 = parseInt(prompt("Ingresa que cantidad de "+ Producto1.nombre + " desea comprar:")) 
-
-        if(cantidadProducto1 <= Producto1.stock){
-            precio(cantidadProducto1, Producto1.precio)
-            alert("El total de su compra es: " + "$" + precioTotal)
-            //Producto1.stock = Producto1.stock - cantidadProducto1
-            Producto1.restarStock(cantidadProducto1)
-            let algoMas = prompt("¿Desea comprar algo mas?: \n- si \n- no")
-            if (algoMas.toLowerCase() == "si"){  // Agregué método del objeto 
-                opcion = prompt("Ingrese que escritorio desea comprar: \n- Escritorio clásico: 1 \n- Escritorio estilo industrial: 2 \n- Escritorio regulable en altura: 3 \n- Salir: 4")
-            }
-            else{
-                break
-            }
-        }
-        else{
-            alert("Por el momento contamos con " + Producto1.stock + " unidades de este escritorio")
-        }
-    }
-    else if(opcion == "2"){
-        let cantidadProducto2 = parseInt(prompt("Ingrese que cantidad de " + Producto2.nombre + " desea comprar:"))
-
-        if(cantidadProducto2 <= Producto2.stock){
-            precio(cantidadProducto2, Producto2.precio)
-            alert("El total de su compra es: " + "$" + precioTotal)
-            //Producto2.stock = Producto2.stock - cantidadProducto2
-            Producto2.restarStock(cantidadProducto2)
-            let algoMas = prompt("¿Desea comprar algo mas?: \n- si \n- no")
-            if (algoMas.toLowerCase() == "si"){  // Agregué método del objeto 
-                opcion = prompt("Ingrese que escritorio desea comprar: \n- Escritorio clásico: 1 \n- Escritorio estilo industrial: 2 \n- Escritorio regulable en altura: 3 \n- Salir: 4")
-            }
-            else{
-                break
-            }
-        }
-        else{
-            alert("Por el momento contamos con " + Producto2.stock + " unidades de este escritorio")
-        }
-    }
-    else if (opcion == "3"){
-        let cantidadProducto3 = parseInt(prompt("Ingrese que cantidad de " + Producto3.nombre + " desea comprar:"))
-
-        if(cantidadProducto3 <= Producto3.stock){
-            precio(cantidadProducto3, Producto3.precio)
-            alert("El total de su compra es: " + "$" + precioTotal)
-            //Producto3.stock = Producto3.stock - cantidadProducto3
-            Producto3.restarStock(cantidadProducto3)
-            let algoMas = prompt("¿Desea comprar algo mas?: \n- si \n- no")
-            if (algoMas.toLowerCase() == "si"){  // Agregué método del objeto 
-                opcion = prompt("Ingrese que escritorio desea comprar: \n- Escritorio clásico: 1 \n- Escritorio estilo industrial: 2 \n- Escritorio regulable en altura: 3 \n- Salir: 4")
-            }
-            else{
-                break
-            }
-        }
-        else{
-            alert("Por el momento contamos con " + Producto3.stock + " unidades de este escritorio")
-        }
-    }
-    else{
-        alert("No contamos con ese producto")
-        opcion = prompt("Ingrese que escritorio desea comprar: \n - Escritorio clásico: 1 \n - Escritorio estilo industrial: 2 \n- Escritorio regulable en altura: 3 \n- Salir: 4")
-    }
-    
-}
-
-// Saludo final
-
-alert("Gracias por visitar nuestro sitio!")
-
-
-
-
-
